da83a025408667dce4344d8f2c44e5460433ef81
itagi99 <mrmonk0707@gmail.com>
2025-08-16 09:51:42 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,20 @@
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('qrupi-store').then(function(cache) {
+    caches.open('upi-qr-cache').then(function(cache) {
       return cache.addAll([
-        '/qstar/',
-        '/qstar/index.html',
-        '/qstar/manifest.json'
-        // add more if needed (like logo file: '/qstar/logo192.png')
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/favicon-192.png',
+        '/favicon-512.png'
       ]);
     })
   );
 });
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
